646486480844e0e01ef645f139aa8d6ea2fbacce
lulek1410 <jan.szewczynski@gmail.com>
2024-03-07 21:10:47 +0100

    (fix): Fix typo

--- models/user.js
+++ models/user.js
@@ -4,7 +4,7 @@ import uniqueValidator from "mongoose-unique-validator";
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true, minlenght: 6 },
+  password: { type: String, required: true, minlength: 6 },
   image: { type: String, required: true },
   places: [{ type: Types.ObjectId, required: true, ref: "Place" }],
 });
